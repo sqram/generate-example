1864415fe79ddead33c8364a499550a38a2c237f
sqram <bruce83@gmail.com>
2019-06-04 16:34:26 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    An example
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  beforeMount ()
+  {
+    console.log('Before mount -- i am in a deferred scripe')
+  }
+}
+</script>
+
